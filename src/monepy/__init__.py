7b45c61d9854d80982fd7738329c23d89dfd21de
Vítor dos Santos Araujo <vitoraraujo@tuta.com>
2025-03-03 21:20:48 -0300

    build: update version number

--- src/monepy/__init__.py
+++ src/monepy/__init__.py
@@ -6,5 +6,5 @@ Currency classes to work with monetary values
 from .currency import BRL, EUR, JPY, USD, GBP
 
 
-__version__ = "0.3"
+__version__ = "0.4"
 __all__ = ["BRL", "EUR", "JPY", "USD", "GBP"]
